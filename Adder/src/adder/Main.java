71f890e832ff8c4366c6502727967380d9257ffe
Nathan Joss (nat543207) <nate@jossfamily.com>
2017-01-30 16:12:34 -0500

    Modified Adder to support an arbitrary number of arguments

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -7,11 +7,18 @@ public class Main {
             int result = addArguments(args);
             System.out.println(result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide any number of integers to add");
         }
     }
 
-    private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+    private static int addArguments(String... args) throws Exception {
+        if(args.length == 0)
+            throw new Exception("No arguments passed");
+
+        int sum = 0;
+        for(String arg : args) {
+            sum += Integer.valueOf(arg);
+        }
+        return sum;
     }
 }
